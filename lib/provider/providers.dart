ceac6a37f9bb73ba74a10dfb065fc39cbc5717f8
Utsab Haldar <utsabhaldar88888@gmail.com>
2024-07-15 16:47:46 +0530

    QR page and state management

---
+++ lib/provider/providers.dart
@@ -0,0 +1,5 @@
+import 'package:riverpod/riverpod.dart';
+
+final upiIdProvider = StateProvider<String>((ref) => '');
+final amountProvider = StateProvider<String>((ref) => '');
+final nameProvider = StateProvider<String>((ref) => '');
